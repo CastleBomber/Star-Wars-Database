00f8637b1841c0910ede242037a5087a072d2112
Maddie Sullivan <matthew.sullivan971@myci.csuci.edu>
2021-05-23 18:24:14 -0700

    added slots for adding every required part of the project

--- Star Wars Database/sql/database_dml_commands.sql
+++ Star Wars Database/sql/database_dml_commands.sql
@@ -1,23 +1,128 @@
 -- 5 Views
+
+-- First View
 create view republic as
 select people_name, force_value
 from warrior join people on
 warrior.people_id = people.people_id
 where faction = 'republic' order by force_value desc;
 
+-- Rationale
+
+
+
+-- Second View
 create view sith as
 select people_name, force_value
 from warrior join people on
 warrior.people_id = people.people_id
 where faction = 'sith' order by force_value desc;
 
+-- Rationale
+
+
+
+-- Third View
+
+-- Rationale
+
+
+
+-- Fourth View
+
+-- Rationale
+
+
+
+-- Fifth View
+
+-- Rationale
+
+
+
+
 
 -- 12 Procedures
+
+-- First Procedure
 Select * from species
 where species = 'yodas_race';
 
+-- Rationale
+
+
+
+-- Second Procedure
+
+-- Rationale
+
+
+
+-- Third Procedure
+
+-- Rationale
+
+
+
+-- Fourth Procedure
+
+-- Rationale
+
+
+
+-- Fifth Procedure
+
+-- Rationale
+
+
+
+-- Sixth Procedure
+
+-- Rationale
+
+
+
+-- Seventh Procedure
+
+-- Rationale
+
+
+
+-- Eighth Procedure
+
+-- Rationale
+
+
+
+-- Ninth Procedure
+
+-- Rationale
+
+
+
+-- Tenth Procedure
+
+-- Rationale
+
+
+
+-- Eleventh Procedure
+
+-- Rationale
+
+
+
+-- Twelfth Procedure
+
+-- Rationale
+
+
+
+
 
 -- 3 Triggers
+
+-- First Trigger
 delimiter //
 create trigger good_guy_points_trigger
 before insert on warriors
@@ -28,3 +133,17 @@ begin
 	end if;
 end; //
 delimiter ;
+
+-- Rationale
+
+
+
+-- Second Trigger
+
+-- Rationale
+
+
+
+-- Third Trigger
+
+-- Rationale
